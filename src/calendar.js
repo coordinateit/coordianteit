988650e12e3c9d45163395ad76366792d2689c75
Ian Strouse <ian.strouse@gmail.com>
2016-10-06 18:12:13 -0600

    Display all visits on calendar

--- src/calendar.js
+++ src/calendar.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
-  $('#calendar').fullCalendar({
 
-  })
+  // GET VISITS FROM SERVER
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: '/user/visitsAll'
+  }).then(function(data) {
+    var visits = data.map(function(i) {
+      let start = new Date(parseInt(i.start));
+      let end = new Date(parseInt(i.start));
+      return {title: i.visittype, start: start, end: end}
+    })
+    initCalendar(visits);
+  });
+
+  // DISPLAY VISITS ON CALENDAR
+  function initCalendar(visits) {
+    $('#calendar').fullCalendar({
+      events: visits
+    });
+  }
 });
