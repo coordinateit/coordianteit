3e4a139b52a8a813e9d617f399d964b81d0ea6e0
Ian Strouse <ian.strouse@gmail.com>
2017-01-10 12:55:30 -0700

    Make map responsive

--- seeds/1-teams.js
+++ seeds/1-teams.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
         knex('teams').insert({ id: 1, team_name: 'Team 1', vehicle: 'F-150' }),
         knex('teams').insert({ id: 2, team_name: 'Team 2', vehicle: 'Mini Cooper' }),
         knex('teams').insert({ id: 3, team_name: 'Team 3', vehicle: 'Tacoma' }),
-        knex('teams').insert({ id: 4,  team_name: 'Team 4', vehicle: 'Tundra' })
+        knex('teams').insert({ id: 4, team_name: 'Team 4', vehicle: 'Tundra' })
       ]);
      });
 };
